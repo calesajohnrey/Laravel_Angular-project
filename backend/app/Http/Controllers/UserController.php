86fa64c05f8db66431ee9dc57c00ca7a594d1098
“Prescilla <21103870@usc.edu.ph>
2023-01-31 11:47:47 +0800

    updated the controller and composer

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use App\User;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        // $user = new User([
+        //     'name' => $request -> get('name'),
+        //     'email' => $request -> get('email'),
+        //     'password' => $request -> get('password')
+        // ]);
+    } 
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injectable } from '@angular/core';
-import { Item } from './Item';
-import { NgForm }   from '@angular/forms';
+// import { User } from './User';
+import { NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 
@@ -8,20 +8,23 @@ import 'rxjs/add/operator/toPromise';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	constructor(private _http: HttpClientModule){}
-        private headers = new Headers({'Content-Type': 'application/json'});
-  	title = 'Laravel Angular 4 App';
-  	onSubmit(form: NgForm): Promise <Item>{
-        return this._http.post('http://127.0.0.1:8000/api/items', JSON.stringify(form.value), {headers: this.headers})
-  		   .toPromise()
-    	           .then(res => res.json().data)
-    	            .catch(this.handleError);
-  }
-  private handleError(error: any): Promise<any> {
-  console.error('An error occurred', error); // for demo purposes only
-  return Promise.reject(error.message || error);
-  }
+  // constructor(private _http: HttpClientModule) {}
+  // private headers = new Headers({ 'Content-Type': 'application/json' });
+  // title = 'Laravel Angular 4 App';
+  // onSubmit(form: NgForm): Promise<Item> {
+  //   return this._http
+  //     .post('http://127.0.0.1:8000/api/items', JSON.stringify(form.value), {
+  //       headers: this.headers,
+  //     })
+  //     .toPromise()
+  //     .then((res) => res.json().data)
+  //     .catch(this.handleError);
+  // }
+  // private handleError(error: any): Promise<any> {
+  //   console.error('An error occurred', error); // for demo purposes only
+  //   return Promise.reject(error.message || error);
+  // }
 }
